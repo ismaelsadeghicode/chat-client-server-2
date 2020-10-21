a7ad4bc8fb8c6e17688029a5744da9f39705f7e5
1qazXSW@ <e.sadeghi@fwutech.com>
2020-10-21 15:30:07 +0330

    online logoff

--- server/src/main/java/com/javatar2/Server.java
+++ server/src/main/java/com/javatar2/Server.java
@@ -1,2 +1,36 @@
-package com.javatar2;public class Server {
+package com.javatar2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server extends Thread {
+    private final int serverPort;
+    private ArrayList<ServerWorker> workerList = new ArrayList<ServerWorker>();
+
+    public List<ServerWorker> getServerWorkers(){
+        return workerList;
+    }
+
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            while (true) {
+                System.out.println("About to accept client connection...");
+                final Socket clientSoket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSoket);
+                ServerWorker serverWorker = new ServerWorker(this, clientSoket);
+                serverWorker.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- server/src/main/java/com/javatar2/ServerWorker.java
+++ server/src/main/java/com/javatar2/ServerWorker.java
@@ -4,12 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
-public class Worker extends Thread {
+public class ServerWorker extends Thread {
     private final Socket clientSoket;
-    private String login =null;
+    private final Server server;
+    private String login = null;
+    private OutputStream outputStream;
 
-    public Worker(Socket clientSoket) {
+    public ServerWorker(Server server, Socket clientSoket) {
+        this.server = server;
         this.clientSoket = clientSoket;
     }
 
@@ -26,7 +30,7 @@ public class Worker extends Thread {
 
     public void handleClientSoket() throws IOException, InterruptedException {
         InputStream inputStream = clientSoket.getInputStream();
-        OutputStream outputStream = clientSoket.getOutputStream();
+        this.outputStream = clientSoket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -34,11 +38,12 @@ public class Worker extends Thread {
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(line)) {
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(line)) {
+                    handleLogoff();
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
-                }else{
+                } else {
                     String msg = "unknown " + cmd + "\n";
                     outputStream.write(msg.getBytes());
                 }
@@ -49,21 +54,63 @@ public class Worker extends Thread {
         clientSoket.close();
     }
 
+    private void handleLogoff() throws IOException {
+        List<ServerWorker> workerList = server.getServerWorkers();
+        // send other online users current user's status
+        String onlineMsg = "offline " + login + "\n";
+        for (ServerWorker worker : workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+        clientSoket.close();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
     private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
-        if(tokens.length == 3){
+        if (tokens.length == 3) {
             String login = tokens[1];
             String password = tokens[2];
 
-            if((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("jim"))){ // login jim jim
+            if ((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("jim"))) { // login jim jim
                 String msg = "Ok Login";
                 outputStream.write(msg.getBytes());
                 this.login = login;
                 System.out.println("User logged in succesfully " + login);
+
+                List<ServerWorker> workerList = server.getServerWorkers();
+
+                // send current user all other online logins
+                for (ServerWorker worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online" + worker.getLogin() + '\n';
+                            send(msg2);
+                        }
+                    }
+                }
+
+                // send other online users current user's status
+                String onlineMsg = "online " + login + "\n";
+                for (ServerWorker worker : workerList) {
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+                    }
+                }
             } else {
                 String msg = "Error Login";
                 outputStream.write(msg.getBytes());
             }
 
+        }
+    }
+
+    private void send(String msg) throws IOException {
+        if (login != null) {
+            outputStream.write(msg.getBytes());
         }
     }
 }
